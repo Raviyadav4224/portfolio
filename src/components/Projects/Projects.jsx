19485941eaf877ad8ee1936a9909eefceeea8e80
Raviyadav4224 <Raviyadav4224@gmail.com>
2024-03-23 18:24:19 +0530

    added project5

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -4,6 +4,7 @@ import project1 from "../../assets/developerImage/project1.png";
 import project2 from "../../assets/developerImage/project2.png";
 import project3 from "../../assets/developerImage/project3.png";
 import project4 from "../../assets/developerImage/project4.png";
+import project5 from "../../assets/developerImage/project5.png";
 import { useSelector } from "react-redux";
 import Github from "../Icons/Github";
 import useScrollTop from "../../misc/useScrollTop";
@@ -32,6 +33,12 @@ const Projects = () => {
       desc: "Fun puzzle game to solve using React",
       projectURL: "https://puzzlecube.onrender.com",
       imageURL: project4,
+    },
+    {
+      title: "Todo App",
+      desc: "Todo List App using Javascript",
+      projectURL: "https://todoapp-50fy.onrender.com",
+      imageURL: project5,
     },
   ];
   const { isDark } = useSelector((state) => state.darkMode);
